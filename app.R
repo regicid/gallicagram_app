eb04e8db137057655b9f716a5a410f91b6198675
benjyazoulay <benjyazoulay@gmail.com>
2021-02-28 21:41:10 +0100

    message mignon pour Benoît

--- app.R
+++ app.R
@@ -30,12 +30,14 @@ Plot <- function(data,input){
   plot = layout(plot, yaxis = y, xaxis = x,title = Title)
   if(length(grep(",",data$mot))==0){plot = layout(plot,showlegend=FALSE)}
   if(input$barplot){
+    Title = paste("<b>Répartition du nombre de numéros présents dans la base\nde données Gallica-presse pour la période", as.character(tableau$date[1])," - ",as.character(tableau$date[length(tableau$date)]))
     width = nrow(tableau)
     span = 2/width + input$span*(width-2)/(10*width)
     tableau$hovers = str_c(tableau$date,": N = ",tableau$base_temp)
     plot1 = plot_ly(tableau, x=~date,y=~base_temp,text=~hovers,type='bar',hoverinfo="text",marker = list(color='rgba(31, 119, 180,1)'))
     y <- list(title = "Nombre de numéros dans Gallica-presse",titlefont = 41)
-    plot1 = layout(plot1,showlegend = FALSE)
+    x <- list(title = data[["resolution"]],titlefont = 41)
+    plot1 = layout(plot1, yaxis = y, xaxis = x,title = Title,showlegend = FALSE)
     plot = subplot(plot,plot1,nrows = 2,legend=NULL,shareX = T)
     return(plot)
   } else{
@@ -44,14 +46,31 @@ Plot <- function(data,input){
 }
 
 get_data <- function(mot,from,to,resolution){
-    mots = str_split(mot,"&")[[1]]
-    tableau<-as.data.frame(matrix(nrow=0,ncol=4),stringsAsFactors = FALSE)
-    progress <- shiny::Progress$new()
-    on.exit(progress$close())
-    progress$set(message = "Patience...", value = 0)
-    for (i in from:to){
+  mots = str_split(mot,"&")[[1]]
+  tableau<-as.data.frame(matrix(nrow=0,ncol=4),stringsAsFactors = FALSE)
+  progress <- shiny::Progress$new()
+  on.exit(progress$close())
+  progress$set(message = "Patience...", value = 0)
+  for (i in from:to){
     for(mot in mots){
       mot2 = str_replace_all(mot," ","%20")
+      ###
+      or<-""
+      or_end<-""
+      if(str_detect(mot2,"[+]")){
+      mots_or = str_split(mot2,"[+]")[[1]]
+      or1<-NA
+      or1_end<-NA
+      for (j in 2:length(mots_or)) {
+        
+        or1[j]<-str_c("or%20text%20adj%20%22",mots_or[j],"%22%20")
+        or1_end[j]<-str_c("%20",mots_or[j])
+        or<-str_c(or,or1[j])
+        or_end<-str_c(or_end,or1_end[j])
+      }
+      mot1<-mots_or[1]}
+      else{mot1=mot2}
+      ###
       end_of_month = c(31,28,31,30,31,30,31,31,30,31,30,31)
       if( i%%4==0){end_of_month[2]=29} #Ne pas oublier les années bisextiles (merci Maxendre de m'y avoir fait penser)
       y<-as.character(i)
@@ -59,26 +78,26 @@ get_data <- function(mot,from,to,resolution){
       end = str_c(y,"/12/31")}
       I = 1
       if(resolution=="Mois"){I=1:12} #Pour faire ensuite une boucle sur les mois
-        for(j in I){
-          if(resolution=="Mois"){
-            z = as.character(j)
-            if(nchar(z)<2){z<-str_c("0",z)}
-            beginning = str_c(y,"/",z,"/01")
-            end = str_c(y,"/",z,"/",end_of_month[j])}
-      url<-str_c("https://gallica.bnf.fr/SRU?operation=searchRetrieve&exactSearch=true&maximumRecords=1&page=1&collapsing=false&version=1.2&query=text%20adj%20%22",mot2,"%22%20%20and%20(dc.type%20all%20%22fascicule%22)%20and%20(gallicapublication_date%3E=%22",beginning,"%22%20and%20gallicapublication_date%3C=%22",end,"%22)%20sortby%20dc.date/sort.ascending&suggest=10&keywords=",mot2)
-      ngram<-as.character(read_xml(url))
-      a<-str_extract(str_extract(ngram,"numberOfRecordsDecollapser&gt;+[:digit:]+"),"[:digit:]+")
-      url_base<-str_c("https://gallica.bnf.fr/SRU?operation=searchRetrieve&exactSearch=true&maximumRecords=1&page=1&collapsing=false&version=1.2&query=(dc.type%20all%20%22fascicule%22)%20and%20(gallicapublication_date%3E=%22",beginning,"%22%20and%20gallicapublication_date%3C=%22",end,"%22)&suggest=10&keywords=")
-      ngram_base<-as.character(read_xml(url_base))
-      b<-str_extract(str_extract(ngram_base,"numberOfRecordsDecollapser&gt;+[:digit:]+"),"[:digit:]+")
-      tableau[nrow(tableau)+1,] = NA
-      date=y
-      if(resolution=="Mois"){date = paste(y,z,sep="/")}
-      tableau[nrow(tableau),]<-c(date,a,b,mot)
+      for(j in I){
+        if(resolution=="Mois"){
+          z = as.character(j)
+          if(nchar(z)<2){z<-str_c("0",z)}
+          beginning = str_c(y,"/",z,"/01")
+          end = str_c(y,"/",z,"/",end_of_month[j])}
+        url<-str_c("https://gallica.bnf.fr/SRU?operation=searchRetrieve&exactSearch=true&maximumRecords=1&page=1&collapsing=false&version=1.2&query=(text%20adj%20%22",mot1,"%22%20",or,")%20%20and%20(dc.type%20all%20%22fascicule%22)%20and%20(ocr.quality%20all%20%22Texte%20disponible%22)%20and%20(gallicapublication_date%3E=%22",beginning,"%22%20and%20gallicapublication_date%3C=%22",end,"%22)&suggest=10&keywords=",mot1,or_end)
+        ngram<-as.character(read_xml(url))
+        a<-str_extract(str_extract(ngram,"numberOfRecordsDecollapser&gt;+[:digit:]+"),"[:digit:]+")
+        url_base<-str_c("https://gallica.bnf.fr/SRU?operation=searchRetrieve&exactSearch=true&maximumRecords=1&page=1&collapsing=false&version=1.2&query=(dc.type%20all%20%22fascicule%22)%20and%20(ocr.quality%20all%20%22Texte%20disponible%22)%20and%20(ocr.quality%20all%20%22Texte%20disponible%22)%20and%20(gallicapublication_date%3E=%22",beginning,"%22%20and%20gallicapublication_date%3C=%22",end,"%22)&suggest=10&keywords=")
+        ngram_base<-as.character(read_xml(url_base))
+        b<-str_extract(str_extract(ngram_base,"numberOfRecordsDecollapser&gt;+[:digit:]+"),"[:digit:]+")
+        tableau[nrow(tableau)+1,] = NA
+        date=y
+        if(resolution=="Mois"){date = paste(y,z,sep="/")}
+        tableau[nrow(tableau),]<-c(date,a,b,mot)
       }
     }
     progress$inc(1/(to-from), detail = paste("Gallicagram ratisse l'an", i))
-    }
+  }
   colnames(tableau)<-c("date","nb_temp","base_temp","mot")
   format = "%Y"
   if(resolution=="Mois"){format=paste(format,"%m",sep="/")}
@@ -92,42 +111,44 @@ get_data <- function(mot,from,to,resolution){
 
 
 ui <- navbarPage("Gallicagram",
-   tabPanel("Graphique",fluidPage(),
-    tags$head(
-    tags$style(HTML(".shiny-output-error-validation{color: red;}"))),
-    pageWithSidebar(headerPanel('Réglages'),
-      sidebarPanel(
-        textInput("mot","Mot à chercher","Mendes-France"),
-        numericInput("beginning","Début",1952),
-        numericInput("end","Fin",1958),
-         sliderInput("span",
-                     "Lissage de la courbe :",
-                     min = 0,
-                     max = 10,
-                     value = 0),
-         selectInput("resolution", label = "Résolution :", choices = c("Année","Mois")),
-        actionButton("do","Générer le graphique"),
-        checkboxInput("barplot", "Afficher la quantité de données", value = FALSE),
-        ),
-      
-            mainPanel(plotlyOutput("plot"),
-         downloadButton('downloadData', 'Télécharger les données')))),
-         tabPanel("Notice",shiny::includeMarkdown("Notice.md")),
-         tabPanel("Corpus",plotlyOutput("corpus"))
-      )
-   
+                 tabPanel("Graphique",fluidPage(),
+                          tags$head(
+                            tags$style(HTML(".shiny-output-error-validation{color: red;}"))),
+                          pageWithSidebar(headerPanel('Réglages'),
+                                          sidebarPanel(
+                                            textInput("mot","Mot à chercher","Mendes-France"),
+                                            numericInput("beginning","Début",1952),
+                                            numericInput("end","Fin",1958),
+                                            sliderInput("span",
+                                                        "Lissage de la courbe :",
+                                                        min = 0,
+                                                        max = 10,
+                                                        value = 0),
+                                            selectInput("resolution", label = "Résolution :", choices = c("Année","Mois")),
+                                            actionButton("do","Générer le graphique"),
+                                            checkboxInput("barplot", "Afficher la quantité de données", value = FALSE),
+                                          ),
+                                          
+                                          mainPanel(plotlyOutput("plot"),
+                                                    headerPanel(""),
+                                                    plotlyOutput("plot1"),
+                                                    downloadButton('downloadData', 'Télécharger les données')))),
+                 tabPanel("Notice",shiny::includeMarkdown("Notice.md")),
+                 tabPanel("Corpus",plotlyOutput("corpus"))
+)
+
 
 
 # Define server logic required to draw a histogram
 server <- function(input, output){
   output$corpus = renderPlotly(Barplot())
-   observeEvent(input$do,{
+  observeEvent(input$do,{
     datasetInput <- reactive({
-        data$tableau})
+      data$tableau})
     df = get_data(input$mot,input$beginning,input$end,input$resolution)
     output$plot <- renderPlotly({Plot(df,input)})
     if(input$barplot){
-    output$plot1 <- renderPlotly({Plot1(df,input)})}
+      output$plot1 <- renderPlotly({Plot1(df,input)})}
     output$downloadData <- downloadHandler(
       filename = function() {
         paste('data-', Sys.Date(), '.csv', sep='')
@@ -135,7 +156,7 @@ server <- function(input, output){
       content = function(con) {
         write.csv(df$tableau, con)
       })
-    })
+  })
   
   
 }
