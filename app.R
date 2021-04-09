5b1f91b263930d31eb20ef0ce4845f052b69a55a
benjyazoulay <benjyazoulay@gmail.com>
2021-04-09 09:58:37 +0200

    corpus_perso

--- app.R
+++ app.R
@@ -494,10 +494,11 @@ server <- function(input, output,session){
       else{output$legende=renderText("Source : gallica.bnf.fr")}
         })})
   output$legende0=renderText("Affichage : Gallicagram par Benjamin Azoulay et Benoît de Courson")
-  
-  output$legende2<-renderText(if(input$doc_type!=4 | input$occurrences_page!=TRUE){str_c(as.character(sum(data[["tableau"]]$base_temp))," documents épluchés\n")}else if(input$doc_type==4 & input$occurrences_page==TRUE){str_c(as.character(sum(data[["tableau"]]$base_count))," pages épluchés\n")})
+  observeEvent(
+    input$occurrences_page,{
+  output$legende2<-renderText(if(input$doc_type!=4 | input$occurrences_page!=TRUE){str_c(as.character(sum(data[["tableau"]]$base_temp))," documents épluchés\n")}else if(input$doc_type==4 & input$occurrences_page==TRUE){str_c(as.character(sum(data[["tableau"]]$base_count))," pages épluchées\n")})
   output$legende3<-renderText(if(input$doc_type!=4 | input$occurrences_page!=TRUE){str_c(as.character(sum(data[["tableau"]]$nb_temp))," résultats trouvés")}else if(input$doc_type==4 & input$occurrences_page==TRUE){str_c(as.character(sum(data[["tableau"]]$count))," pages correspondant à la recherche")})
-
+})
  
   output$downloadData <- downloadHandler(
     filename = function() {
